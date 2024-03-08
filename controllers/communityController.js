2469fa7ce67971daded81a258a7fd65c004f3244
dipanshuD09 <dipanshud146@gmail.com>
2024-03-08 20:24:53 +0530

    final

--- controllers/communityController.js
+++ controllers/communityController.js
@@ -55,39 +55,47 @@ const getMyCommunities = asyncHandler(async (req, res) => {
 const createCommunity = asyncHandler(async (req, res) => {
   const { name } = req.body;
   const admin = await Role.findOne({ name: "Community Admin" });
-  if (!name) {
-    res.status(400);
-    throw new Error("Community must have a name");
+  const comm = await Community.findOne({ name });
+  if (!comm) {
+    if (!name) {
+      res.status(400);
+      throw new Error("Community must have a name");
+    } else {
+      const community = new Community({
+        name,
+        slug: name.toLowerCase(),
+        owner: req.user._id,
+      });
+      const member = new Member({
+        community: community._id,
+        user: req.user._id,
+        role: admin._id,
+      });
+      await member.save();
+      const createdCommunity = await community.save();
+      res.status(201).json({
+        status: true,
+        content: {
+          data: createdCommunity,
+        },
+      });
+    }
   } else {
-    const community = new Community({
-      name,
-      slug: name.toLowerCase(),
-      owner: req.user._id,
-    });
-    const member = new Member({
-      community: community._id,
-      user: req.user._id,
-      role: admin._id,
-    });
-    await member.save();
-    const createdCommunity = await community.save();
-    res.status(201).json({
-      status: true,
-      content: {
-        data: createdCommunity,
-      },
-    });
+    res.status(401);
+    throw new Error(
+      "Community with provided name already in existence"
+    );
   }
 });
 
 const getAllCommunities = asyncHandler(async (req, res) => {
   const comm = await Member.find({ user: req.user._id });
-  
+
   const comms = comm.map((c) => c.community);
-  
+
   const communityPromises = comms.map(async (c) => await Community.findById(c));
   const communities = await Promise.all(communityPromises);
-  
+
   const pages = Math.ceil(communities.length / 10);
   res.json({
     status: true,
